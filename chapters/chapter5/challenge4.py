01027cc274d1c340b66c0e5aa5d3a977ede6b73b
unknown <kdhale3953@GIS-VLAB-9.ad.uauth.net>
2015-09-25 18:45:27 -0400

    Available extensions
    This code let's you know which extensions are available for use in ArcGIS

---
+++ chapters/chapter5/challenge4.py
@@ -0,0 +1,10 @@
+"""This script determines whether the following extensions are available: 
+ArcGIS 3D Analyst, ArcGIS Network Analyst, and ArcGIS Spatial
+Analyst."""
+
+
+def available extensions raw_input():
+	if ArcGIS is available:
+		print "Yes"
+
+	
